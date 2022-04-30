559df641a05220a74a08773ee085c70056b81d62
pilartkz <pilar.tkz@gmail.com>
2022-04-30 16:36:26 +0200

    Cambios

--- CCuenta.java
+++ CCuenta.java
@@ -4,7 +4,7 @@ public class CCuenta {
     private String nombre;
     private String cuenta;
     private double saldo;
-    private double tipoInterés;
+    private double tipoInteres;
 
     public CCuenta()
     {
@@ -12,21 +12,21 @@ public class CCuenta {
 
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
-        nombre =nom;
-        cuenta=cue;
-        saldo=sal;
+        setNombre(nom);
+        setCuenta(cue);
+        setSaldo(sal);
     }
 
     public double estado()
     {
-        return saldo;
+        return getSaldo();
     }
 
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
-        saldo = saldo + cantidad;
+        setSaldo(getSaldo() + cantidad);
     }
 
     public void retirar(double cantidad) throws Exception
@@ -35,6 +35,62 @@ public class CCuenta {
             throw new Exception ("No se puede retirar una cantidad negativa");
         if (estado()< cantidad)
             throw new Exception ("No se hay suficiente saldo");
-        saldo = saldo - cantidad;
+        setSaldo(getSaldo() - cantidad);
     }
+
+	/**
+	 * @return the nombre
+	 */
+	private String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	private void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	/**
+	 * @return the cuenta
+	 */
+	private String getCuenta() {
+		return cuenta;
+	}
+
+	/**
+	 * @param cuenta the cuenta to set
+	 */
+	private void setCuenta(String cuenta) {
+		this.cuenta = cuenta;
+	}
+
+	/**
+	 * @return the saldo
+	 */
+	private double getSaldo() {
+		return saldo;
+	}
+
+	/**
+	 * @param saldo the saldo to set
+	 */
+	private void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	/**
+	 * @return the tipoInteres
+	 */
+	private double getTipoInteres() {
+		return tipoInteres;
+	}
+
+	/**
+	 * @param tipoInteres the tipoInteres to set
+	 */
+	private void setTipoInteres(double tipoInteres) {
+		this.tipoInteres = tipoInteres;
+	}
 }
